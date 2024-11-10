f2fcc7851f9786b8f2e0e1a76ac01cf6a239f449
Magomed <m-basir@bk.ru>
2024-11-10 09:52:29 +0300

    world_time final changes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,18 +17,40 @@ class _HomeState extends State<Home> {
     data = ModalRoute.of(context)?.settings.arguments as Map<dynamic, dynamic>;
     print(data);
 
+    // set background
+    String bgImage = data['isDayTime'] ? 'isDay.webp' : 'isNight.webp';
+    Color bgColor = data['isDayTime']
+        ? Colors.green[500] ?? Colors.green
+        : Colors.indigo[800] ?? Colors.indigo;
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-          child: Column(
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+              child: Column(
+                 children: [
+                      TextButton.icon(
+                         onPressed: () {
                   Navigator.pushNamed(context, '/location');
                 },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location'),
+                icon: Icon(
+                    Icons.edit_location,
+                  color: Colors.grey[300],
+                ),
+                label: Text(
+                    'Edit Location',
+                  style: TextStyle(
+                      color: Colors.grey[300]
+                  ),
+                ),
               ),
               SizedBox(height: 20.0),
               Row(
@@ -39,6 +61,7 @@ class _HomeState extends State<Home> {
                     style: TextStyle(
                       fontSize: 28.0,
                       letterSpacing: 2.0,
+                      color: Colors.white,
                       fontWeight: FontWeight.bold
                     )
                   ),
@@ -49,11 +72,13 @@ class _HomeState extends State<Home> {
                 data['time'],
                 style: TextStyle(
                   fontSize: 60.0,
+                    color: Colors.white,
                     fontWeight: FontWeight.bold
                 ),
               )
             ],
           ),
+        ),
         ),
       ),
     );
